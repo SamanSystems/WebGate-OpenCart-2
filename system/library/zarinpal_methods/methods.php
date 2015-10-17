e04807f3b9cd214ce3dff81adcf7e768ace58084
Masoud A <amini141@gmail.com>
2015-10-17 12:04:57 +0330

    Update methods.php

--- system/library/zarinpal_methods/methods.php
+++ system/library/zarinpal_methods/methods.php
@@ -1,7 +1,7 @@
 <?php
     function Request($MerchantID,$Amount,$Description,$CallbackURL){
 		// URL also Can be https://ir.zarinpal.com/pg/services/WebGate/wsdl
-		$client = new SoapClient('https://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); 
+		$client = new SoapClient('https://ir.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); 
 		
 		$result = $client->PaymentRequest(
 							array(
@@ -20,7 +20,7 @@
   
     function Verification($MerchantID,$Amount,$Authority){
 		// URL also Can be https://ir.zarinpal.com/pg/services/WebGate/wsdl
-		$client = new SoapClient('https://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); 
+		$client = new SoapClient('https://ir.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); 
 		
 		$result = $client->PaymentVerification(
 						  	array(
